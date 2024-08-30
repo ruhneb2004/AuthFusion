1f93cafcfbd6002f9eadf65b2a9bbe05cf52d6e1
benhur <benhurbenny@iclould.com>
2024-08-30 21:26:04 +0530

    completed the front page

---
+++ packages/nextjs/app/assets/font.ts
@@ -0,0 +1,13 @@
+import { Italiana, Pacifico } from "next/font/google";
+
+const italiana = Italiana({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const pacifico = Pacifico({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export { italiana, pacifico };
